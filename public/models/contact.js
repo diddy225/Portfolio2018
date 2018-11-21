bd2038568ba4ae3ca893bc68752e003ab4176063
Matthew C <matthew.j.carpenter1@gmail.com>
2018-11-21 09:17:22 -0500

    updated files

--- public/models/contact.js
+++ public/models/contact.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-
+//comment box
 let ContactSchema = new Schema({
 
     firstName: {
--- public/routes/html-routes.js
+++ public/routes/html-routes.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = function(app) {
-
+  //landing page
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
